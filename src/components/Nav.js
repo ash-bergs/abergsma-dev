2acd1757e1b7ad0518ce0df9a97bd2b9df6a5209
Ash <ashley.bergsma42@gmail.com>
2021-01-06 15:08:15 -0700

    Adds common components/nav
    Nav structure and a couple of common components added to the project.

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+
+import Container from './common_components/Container'; 
+
+function Nav() {
+
+    return(
+        <nav>
+            <Container>
+                <h2>Ashley Bergsma</h2>
+            </Container>
+        </nav>
+    );
+}
+
+export default Nav; 
