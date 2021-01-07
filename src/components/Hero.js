ad728fcbee5fc4b6dda485f7640997c87cc345f0
Ash <ashley.bergsma42@gmail.com>
2021-01-06 20:08:32 -0700

    Adds basic flexbox helper classes
    Helper classes have been added to the css. Before I get ahead of myself though, I'm going to add Sass to the project, and sketch out the hierarchy of the components.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,14 @@
 import React from 'react'; 
 
 import Container from './common_components/Container'; 
+import Headline from './Headline'; 
 
 function Hero() {
 
     return( 
         <section className="hero">
-            <Container>
                 {/* a box featuring my name and a sunflower animation will go here */}
-            </Container>
+            <Headline />
             <Container>
                 {/* another section will be here - detailed about me, and image carousel */}
             </Container>
