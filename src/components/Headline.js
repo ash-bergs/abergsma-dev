339be5f69aa2f4a9cc6f728d35754f192f58d0ec
Ash <ashley.bergsma42@gmail.com>
2021-01-08 13:14:15 -0700

    Adds header image
    Adds header image and applies some CSS classes and rules to elements.

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -4,7 +4,7 @@ function Headline() {
 
     return(
         <section className="headlineBg">
-            <div className="flexBox column">
+            <div className="flexBox column hero_star">
                 <h1>Ashley Bergsma</h1>
                 <p>I'm a full stack developer</p>
             </div>
