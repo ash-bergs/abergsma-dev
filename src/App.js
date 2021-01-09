25dbdb326405ff58c8df43722025f178d112df2b
Ash <ashley.bergsma42@gmail.com>
2021-01-09 16:00:43 -0700

    Adds Headline styles
    Sass has been fully added to the project, App.css has been removed.
    
    The name in the Headline component has been styled using CSS positioning and flexbox.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'; 
 
-//import './sass/App.scss';
-import './App.css'; 
+import './sass/App.scss';
 
 import Hero from './components/Hero'; 
 import Nav from './components/Nav'; 
@@ -13,6 +12,8 @@ function App() {
     <Router>
       <Nav />
       <Route exact path="/" component={Hero} /> 
+      {/* //TODO Route path="/contact" */}
+      {/* //TODO Route path="/seeMore" Not sure on the path name yet - a place to list social medias and interesting dev social platforms */}
     </Router>
   );
 }
