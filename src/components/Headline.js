1698d0edb0ea19d1317331578e00c36472a89542
Ash <ashley.bergsma42@gmail.com>
2021-01-08 20:39:42 -0700

    Adds fonts
    Fonts to be used in the project have been added to public.html
    
    Now that some position and hierarchy has been established it might be time to add some SASS to the project and better organize the style files.
    
    README updated to reflect style choices

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -4,9 +4,11 @@ function Headline() {
 
     return(
         <section className="headlineBg">
-            <div className="flexBox column hero_star">
-                <h1>Ashley Bergsma</h1>
-                <p>I'm a full stack developer</p>
+            <div className="flexBox column header">
+                <div class="innerContent">
+                    <h1 class="title headline"><span class="top-stagger">Ashley</span> <span class="bottom-stagger">Bergsma</span></h1>
+                    <p>I'm a full stack developer</p>
+                </div>
             </div>
         </section>
     );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,7 +8,7 @@ function Nav() {
         <nav>
             <Container>
                 <div className="wrapper flexBox">
-                    <h2 className="flexGrow_2">Ashley Bergsma</h2>
+                    <h2 className="flexGrow_2 title navAnchor">Ashley Bergsma</h2>
                     <p className="navItem">About</p>
                     <p className="navItem">Blog</p>
                     <p className="navItem">Contact</p>
