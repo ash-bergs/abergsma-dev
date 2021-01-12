129b055e93c31d1aa3a6f9e19e83f8b1a44edacb
Ash <ashley.bergsma42@gmail.com>
2021-01-12 10:49:54 -0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ function About() {
                 <div className="col-1-of-2 u-center-content">
                     <span className="headline section-subtitle">I'm a full stack engineer based in Phoenix, Arizona.</span>
                     <p>I'm a creative developer, with a passion for making and keeping the internet accessible and beautiful for all users. </p>
-                    <p>Though I'm proficient with the MERN stack, learning a new language or library is a welcome challenge. Currently I'm learning about NextJs and Lottie. My journey to master React and JavaScript while learning new patterns and skills can be followed on my <a href="">blog</a></p>
+                    <p>Though I'm proficient with the MERN stack, learning a new language or library is a welcome challenge. Currently I'm learning about NextJs and Lottie. My journey to master React and JavaScript while learning new patterns and skills can be followed on my <a href="https://dev.to/ash_bergs">blog</a></p>
                 </div>
                 <div className="col-1-of-2">
                     <p>Image carousel goes here 🎠</p>
