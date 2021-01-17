de0024744ae93380e8d138199f40ae3b33ed3dbc
Ash <ashley.bergsma42@gmail.com>
2021-01-16 21:24:35 -0700

    Adds skills card styling
    Fontawesome was added to the program, and some animation was added to the new skills card section!

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -3,9 +3,9 @@ import React from 'react';
 function SkillCard({ card }) {
     return (
         <div className="skill-card">
-            <i className=""></i>
-            <h3 className="headline skill-title"> {card.title} </h3>
-            <ul>
+            <i className="fas fa-book feature-icon"></i>
+            <h3 className="headline section-subtitle"> {card.title} </h3>
+            <ul className="skill-list">
                 {card.featuredSkills.map((skill, index) => (
                     <li key={index}> {skill} </li>
                 ))}
