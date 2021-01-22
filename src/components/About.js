5b2047a69f0573fc679e034d4b987ae0bb9e9b5c
Ash <ashley.bergsma42@gmail.com>
2021-01-22 10:18:30 -0700

    Updates About section
    Changed the wording and some typography. Moved a redundant rule from base.scss to the typography file, text content in about section is being controlled by about-text class name

--- src/components/About.js
+++ src/components/About.js
@@ -10,8 +10,8 @@ function About() {
             <div className="row">
                 <div className="col-1-of-2 u-center-content u-increase-height">
                     <span className="headline section-subtitle">I'm a <span className="highlighted">full stack engineer</span> based in Phoenix, Arizona.</span>
-                    <p>I'm a creative developer, with a passion for making and keeping the internet accessible and beautiful for all users. </p>
-                    <p>Though I'm proficient with the MERN stack, learning a new language or library is a welcome challenge. Currently I'm learning about NextJs and Lottie. My journey to master React and JavaScript while learning new patterns and skills can be followed on my <a href="https://dev.to/ash_bergs">blog</a></p>
+                    <p className="about-text">I have a passion for creating beautiful and accessible web and mobile applications. I believe the internet is for everyone, and accessibility-minded design makes it that way.</p>
+                    <p className="about-text">I'm proficient with the MERN stack, but learning new libraries and languages are a welcome challenge. As a developer my journey to learn is never over, I'm currently tackling NextJs and LottieFiles. </p>
                 </div>
                 <div className="col-1-of-2 u-increase-margin-top u-increase-height">
                     <Container>
