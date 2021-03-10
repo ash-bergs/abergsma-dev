8264f3458726a7f122586253e45e7942ce6fcd9a
Ash <ashley.bergsma42@gmail.com>
2021-03-10 14:06:29 -0700

    Image optimization properties added to image carousel
    `loading="lazy"` and `decoding="async"` added to make the page a little faster to load (for browsers that have support for them). I need to compress the actual images themselves too!

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -6,7 +6,15 @@ function ImageCarousel() {
     return (
         <div className="composition">
             {projectsData.map((project, index) => (
-                <a href={project.href} key={index}> <img src={project.src} alt="project snap" className={project.className} /> </a>
+                <a href={project.href} key={index}> 
+                <img 
+                    src={project.src} 
+                    loading="lazy"
+                    decoding="async"
+                    alt="project screenshot" 
+                    className={project.className}
+                /> 
+                </a>
             ))}
         </div>
     ); 
