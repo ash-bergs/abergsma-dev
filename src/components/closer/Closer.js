0efd90822bae6cfc9368b9c154f160a307252288
Ash <ashley.bergsma42@gmail.com>
2021-03-14 14:32:42 -0700

    Adds SocialBar component
    Various updates and a new component has been added to the mix. Though it's not complete (and therefore not being rendered), it will see completion in the next 48 hours.

--- src/components/closer/Closer.js
+++ src/components/closer/Closer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+// import SocialBar from './SocialBar'; 
 import Typer from '../common_components/Typer'; 
 
 function Closer() {
@@ -21,7 +22,8 @@ function Closer() {
             <div className="closing-statement">
                 <p className="small-text">The developer's journey to learn is never over, and while I'm most proficient with the M.E.R.N. stack, I'll never shy from the challenge of undertaking a new language, convention or project. I maintain a blog documenting my journey to master my primary language, JavaScript, and the subjects I really love - like CSS, best practices, the art of reading code, time management and more.</p>
                 <br/>
-                <p className="small-text"> I love working on teams! I know that to build a team environment I have to be flexible, communicative and thoughtful in my feedback. I have experience working on fully remote teams, filling both back and frontend roles. These teams used Agile methodologies to coordinate team effort, and I've since implemented many of those approaches into my personal workflow. If you're interested in hearing what peers have to say about working with me, please see my SearchLight profile, linked below.</p>
+                <p className="small-text"> I love working on teams! I know that to build a team environment means being flexible, communicative and thoughtful in my feedback. I have experience working on fully remote teams, filling both back and frontend roles. These teams used Agile methodologies to coordinate team effort, and I've since implemented many of those approaches into my personal workflow. If you're interested in hearing what peers have to say about working with me, please see my SearchLight profile, linked below.</p>
+                {/* SOCIAL BAR RENDERING HERE WHEN COMPLETE */}
             </div>
         </div>
     )
