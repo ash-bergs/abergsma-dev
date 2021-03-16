2f72f78d82f23ed4c5e6500bbb34ad1162303715
Ash <ashley.bergsma42@gmail.com>
2021-03-16 16:18:17 -0700

    Updates Skills Cards
    Here we are again! Making the skills card a little more benign on the page. Added media queries to render in a column on phone screen

--- src/components/skills/SkillCard.js
+++ src/components/skills/SkillCard.js
@@ -4,7 +4,7 @@ function SkillCard({ card }) {
     return (
         <div className="skill-card">
             <i className={card.icon}></i>
-            <h3 aria-label="skill class" className="headline card-title"> {card.title} </h3>
+            <h3 aria-label="skill class" className="card-title"> {card.title} </h3>
             <ul className="skill-list">
                 {card.featuredSkills.map((skill, index) => (
                     <li aria-label="skill name" key={index}> {skill} </li>
