72dffcf9d65eb8a1e9b71da7f34008bb45ee11ce
Ash <ashley.bergsma42@gmail.com>
2021-04-19 19:22:55 -0700

    Removes unneeded icons
    Removed icons from the Socials component directory

--- src/App.js
+++ src/App.js
@@ -26,15 +26,3 @@ function App() {
 
 export default App;
 
-
-//! Sticking point: I was setting up the ContactForm component, trying to connect it to the Nav bar 
-// When contact is clicked, the route should change, and a contact form should appear 
-//! The problem: The route was changing, but the component wasn't rendering 
-//* What I think it is: It just hit me! I think I need a Switch! Duh! 
-//* it seems to have worked! I also changed the rendering method in the main Router here in App 
-// Now I must add a third party library - 
-
-
-// Notes on the React Router Switch component: 
-//* Renders a route *exlusively* 
-
